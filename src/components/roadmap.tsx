61f544561ed1da04331c06141c3af5b05a9de629
Edoo29 <edoardo.zaffaroni@gmail.com>
2025-04-20 12:05:50 +0200

    finished the home section

--- src/components/roadmap.tsx
+++ src/components/roadmap.tsx
@@ -1,60 +1,7 @@
-import { FaArrowDown } from "react-icons/fa";
-import { RoadmapItemProps, RoadmapItemType } from "../types";
+import { FaArrowDown, FaArrowUp, FaArrowsAltV } from "react-icons/fa";
+import { RoadmapItemProps } from "../types";
 import { motion } from "motion/react";
-
-const roadmapItems: RoadmapItemType[] = [
-  {
-    year: 2022,
-    title: "Start learning HTML and CSS",
-    description:
-      "I started to study HTML and CSS by following videos on youtube",
-    color: "black",
-  },
-  {
-    year: 2023,
-    title: "Tried to study something else",
-    description: "I started to study C++ and Python to find what I like more",
-    color: "blue",
-  },
-  {
-    year: 2023,
-    title: "Started to study JavaScript",
-    description: "I started to study JavaScript",
-    color: "orange",
-  },
-  {
-    year: 2023,
-    title: "Improvements",
-    description: "I saw something about React and started TailwindCSS",
-    color: "red",
-  },
-  {
-    year: 2024,
-    title: "Improved HTML and CSS",
-    description:
-      "I improved my HTML and CSS knowledge studying SEO and Accessibility",
-    color: "green",
-  },
-  {
-    year: 2024,
-    title: "Started to study React and TypeScript",
-    description: "I started to study React and TypeScript to improve my skills",
-    color: "pink",
-  },
-  {
-    year: 2024,
-    title: "Discovered new technologies",
-    description:
-      "Tried to study more technologies like Next.js, DBs with ORMs and more",
-    color: "violet",
-  },
-  {
-    year: 2025,
-    title: "In-depth study of React, Typescript and SCSS",
-    description: "I started to study Node.js to improve my skills",
-    color: "black",
-  },
-];
+import { roadmapItems } from "../data";
 
 export default function Roadmap() {
   return (
@@ -71,14 +18,25 @@ export default function Roadmap() {
   );
 }
 
-function RoadmapItem({ year, title, description, color }: RoadmapItemProps) {
+function RoadmapItem({
+  year,
+  title,
+  description,
+  color,
+  idx,
+}: RoadmapItemProps) {
   return (
     <div className={`roadmap-item ${color}`}>
       <h2>{year}</h2>
       <h3>{title}</h3>
       <p>{description}</p>
-
-      <FaArrowDown className="scroll-down-arrow" />
+      {idx === 0 ? (
+        <FaArrowDown className="arrow" />
+      ) : idx === roadmapItems.length - 1 ? (
+        <FaArrowUp className="arrow" />
+      ) : (
+        <FaArrowsAltV className="arrow" />
+      )}
     </div>
   );
 }
---
+++ src/data.ts
@@ -0,0 +1,56 @@
+import { RoadmapItemType } from "./types";
+
+export const roadmapItems: RoadmapItemType[] = [
+  {
+    year: 2022,
+    title: "Start learning HTML and CSS",
+    description:
+      "I started to study HTML and CSS by following videos on youtube",
+    color: "black",
+  },
+  {
+    year: 2023,
+    title: "Tried to study something else",
+    description:
+      "I did this because I wanted to learn something new and different",
+    color: "blue",
+  },
+  {
+    year: 2023,
+    title: "Started to study JavaScript",
+    description: "I returned to study web development with JavaScript",
+    color: "orange",
+  },
+  {
+    year: 2023,
+    title: "Improvements",
+    description: "I saw something about React and TailwindCSS",
+    color: "red",
+  },
+  {
+    year: 2024,
+    title: "Improved HTML and CSS",
+    description:
+      "I improved my HTML and CSS knowledge studying SEO and Accessibility",
+    color: "green",
+  },
+  {
+    year: 2024,
+    title: "Started to study React and TypeScript",
+    description: "I started to study React and TypeScript to improve my skills",
+    color: "pink",
+  },
+  {
+    year: 2024,
+    title: "Discovered new technologies",
+    description:
+      "I tried to study more technologies like Next.js, DBs with ORMs and more",
+    color: "violet",
+  },
+  {
+    year: 2025,
+    title: "In-depth study of React, Typescript and SCSS",
+    description: "I improved my knowledge of React, Typescript and SCSS",
+    color: "black",
+  },
+];
